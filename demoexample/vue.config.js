074fca55549a3fe96dc89fee890e922320dd7689
Sergio Valdez <valdezsergio@hotmail.com>
2024-04-04 13:20:02 -0600

    0102

--- demoexample/vue.config.js
+++ demoexample/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+const webpack = require('webpack');
+
+module.exports = defineConfig(
+  {
+    configureWebpack:{
+      plugins:[
+        new webpack.DefinePlugin({
+          __VUE_PROD_HYDRATION_MISMATCH_DETAILS__:'false',
+        })
+      ]
+    }
+  },
+  {
   transpileDependencies: true
 })
